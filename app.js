73c6381e614a8964cb9efdfbace15de140db6406
“muhammad <muhammadsamik681@gmail.com>
2024-11-23 01:43:04 +0500

    origin main

---
+++ app.js
@@ -0,0 +1,56 @@
+// //question1
+
+// var great  ="your best website" 
+// alert(great)
+
+// //question2
+// var browserShow = "Enter a valid  password"
+// alert(browserShow);
+
+// //question3
+
+// var alert1 = "welcome to js"  
+// var alert2 =  "land happy coding"
+// alert(alert1 +alert2 );
+
+// //question 4
+// var messege = "Hello I can run throught my web browser console"
+// console.log(messege);
+
+
+// //question5
+
+
+// //asigment 2
+
+// question1
+// var userName = "muhammadsami"
+// console.log(userName);
+
+// //question2
+// var myName ="sami"
+// console.log(myName);
+
+// //question3
+// var messege = "helloworld"
+// alert(messege)
+
+
+// //question4
+// var user_name = "asfaq"
+// var userAge = 19
+// var skills = "Web development"
+// console.log(user_name + userAge +skills);
+
+
+// //question5
+// var userEmail = "muhammadsamik681@gmail.com"
+// console.log(userEmail);
+
+// //question6
+// var book = "I am trying A smart way to learn javascript"
+// console.log(book);
+
+// question7
+var browser ="I can write HTMl content thought javacript"
+document.write(browser)
