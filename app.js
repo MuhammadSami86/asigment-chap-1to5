00467536c20ee5b008c4d3a3bbe9371ce5c70a29
“muhammad <muhammadsamik681@gmail.com>
2024-11-24 15:36:38 +0500

    origin main

--- app.js
+++ app.js
@@ -1,143 +1,163 @@
-//question1
+// //question1
 
-var great  ="your best website" 
-alert(great)
+// var great  ="your best website" 
+// alert(great)
 
-//question2
-var browserShow = "Enter a valid  password"
-alert(browserShow);
+// //question2
+// var browserShow = "Enter a valid  password"
+// alert(browserShow);
 
-//question3
+// //question3
 
-var alert1 = "welcome to js"  
-var alert2 =  "land happy coding"
-alert(alert1 +alert2 );
+// var alert1 = "welcome to js"  
+// var alert2 =  "land happy coding"
+// alert(alert1 +alert2 );
 
-//question 4
-var messege = "Hello I can run throught my web browser console"
-console.log(messege);
-
-
-//question5
+// //question 4
+// var messege = "Hello I can run throught my web browser console"
+// console.log(messege);
 
 
-//asigment 2
-
-//question1
-var userName = "muhammadsami"
-console.log(userName);
-
-//question2
-var myName ="sami"
-console.log(myName);
-
-//question3
-var messege = "helloworld"
-alert(messege)
+// //question5
 
 
-//question4
-var user_name = "asfaq"
-var userAge = 19
-var skills = "Web development"
-console.log(user_name + userAge +skills);
+// //asigment 2
+
+// //question1
+// var userName = "muhammadsami"
+// console.log(userName);
+
+// //question2
+// var myName ="sami"
+// console.log(myName);
+
+// //question3
+// var messege = "helloworld"
+// alert(messege)
 
 
-//question5
-var userEmail = "muhammadsamik681@gmail.com"
-console.log(userEmail);
-
-//question6
-var book = "I am trying A smart way to learn javascript"
-console.log(book);
-
-// question7
-var browser ="I can write HTMl content thought javacript"
-document.write(browser)
-
-//chapter 3
-question1
-var age = 19;
-alert("your age is"+age)
-
-//question2
-var visiterUse = 14
-console.log("you have visted this side "+visiterUse);
-
-//question3
-var myBirthYear = 2005;
-document.write("my Birth Year is "+myBirthYear)
-//question4
-var clientName = "muhammad sami"
-var clientProduct = "5 T-shirt"
-var clientQuantity = "55"
-document.write(clientName + "is "+clientProduct + "on xyz clothing store "+clientQuantity)
-
-//chapter4
-// question1
-var userName ="sami",fatherName = "abdulQadir",age = 19
-console.log(userName+ " "+ fatherName +" "+age);
+// //question4
+// var user_name = "asfaq"
+// var userAge = 19
+// var skills = "Web development"
+// console.log(user_name + userAge +skills);
 
 
-//question2
-// illegal variable
-// 1 : var user name //spacing
-// 2var 1 = 1 //number is not
-// 3var First = "name" //first letter capital not
-// 4 var alert = "sami" //javascript keyword 
-// 5var % = "sami"// dollor or underscores ke ilawa na hon
+// //question5
+// var userEmail = "muhammadsamik681@gmail.com"
+// console.log(userEmail);
+
+// //question6
+// var book = "I am trying A smart way to learn javascript"
+// console.log(book);
+
+// // question7
+// var browser ="I can write HTMl content thought javacript"
+// document.write(browser)
+
+// //chapter 3
+// //question1
+// var age = 19;
+// alert("your age is"+age)
+
+// //question2
+// var visiterUse = 14
+// console.log("you have visted this side "+visiterUse);
+
+// //question3
+// var myBirthYear = 2005;
+// document.write("my Birth Year is "+myBirthYear) 
+// //question4
+// var clientName = "muhammad sami"
+// var clientProduct = "5 T-shirt"
+// var clientQuantity = "55"
+// document.write(clientName + "is "+clientProduct + "on xyz clothing store "+clientQuantity)
+
+// //chapter4
+// // question1
+// var userName ="sami",fatherName = "abdulQadir",age = 19
+// console.log(userName+ " "+ fatherName +" "+age);
 
 
-//chapter 5
-//question 1
-var firstNum = 3
-var secondNum = 5
-var total = firstNum + secondNum
-console.log("sum of" +3+ "and"+secondNum +"is"+ total );
+// //question2
+// // illegal variable
+// // 1 : var user name //spacing
+// // 2var 1 = 1 //number is not
+// // 3var First = "name" //first letter capital not
+// // 4 var alert = "sami" //javascript keyword 
+// // 5var % = "sami"// dollor or underscores ke ilawa na hon
 
 
-//question 2
-var firstNum = 3
-var secondNum = 5
-var total = firstNum - secondNum
-console.log("sum of" +3+ "and"+secondNum +"is"+ total );
+// //chapter 5
+// //question 1
+// var firstNum = 3
+// var secondNum = 5
+// var total = firstNum + secondNum
+// console.log("sum of" +3+ "and"+secondNum +"is"+ total );
 
-var firstNum = 3
-var secondNum = 5
-var total = firstNum * secondNum
-console.log("sum of" +3+ "and"+secondNum +"is"+ total );
 
-var firstNum = 3
-var secondNum = 5
-var total = firstNum / secondNum
-console.log("sum of" +3+ "and"+secondNum +"is"+ total );
+// //question 2
+// var firstNum = 3
+// var secondNum = 5
+// var total = firstNum - secondNum
+// console.log("sum of" +3+ "and"+secondNum +"is"+ total );
 
-//question 3
-  var mathExpres = 5;
-  console.log( "initaialze value"+mathExpres);
+// var firstNum = 3
+// var secondNum = 5
+// var total = firstNum * secondNum
+// console.log("sum of" +3+ "and"+secondNum +"is"+ total );
+
+// var firstNum = 3
+// var secondNum = 5
+// var total = firstNum / secondNum
+// console.log("sum of" +3+ "and"+secondNum +"is"+ total );
+
+// //question 3
+//   var mathExpres = 5;
+//   console.log( "initaialze value"+mathExpres);
   
-  mathExpres++
- console.log("increment value "+ mathExpres);
+//   mathExpres++
+//  console.log("increment value "+ mathExpres);
  
-var addNum = 7
-var plus = mathExpres + addNum
+// var addNum = 7
+// var plus = mathExpres + addNum
 
-console.log("after addition"+ plus);
+// console.log("after addition"+ plus);
 
-plus--
-console.log("after decrement"+plus);
+// plus--
+// console.log("after decrement"+plus);
 
-var divide = plus%3
-console.log("this is reminder value  " +divide);
+// var divide = plus%3
+// console.log("this is reminder value  " +divide);
 
-console.log(divide);
+// console.log(divide);
 
-//question4
-var ticketPrice  = 6000;
-var fiveTicketPrice  = ticketPrice*5
-console.log("five ticket price"+fiveTicketPrice);
+// //question4
+// var ticketPrice  = 6000;
+// var fiveTicketPrice  = ticketPrice*5
+// console.log("five ticket price"+fiveTicketPrice);
 
-//question5
-for(var i =1;i<11;i++){
-    console.log("4 x"+i  +"="+i *4 );
-    }
+// //question5
+// for(var i =1;i<11;i++){
+//     console.log("4 x"+i  +"="+i *4 );
+//     }
+
+////question 7
+// var priceItemOne = 750;
+// var priceItemTwo = 850;
+// var quantityItemOne = 3
+// var quantityItemTwo = 5
+// var shippingCharges = 100
+// var totalCostOne = priceItemOne *quantityItemOne +100 
+// var totalCostTwo = priceItemTwo*quantityItemTwo +shippingCharges
+// document.write("price of item 1 is"+priceItemOne +"<br>"+"quantity Of item one"+quantityItemOne +"<br>"+"shipping charges"+shippingCharges),"<br>"
+// document.write("totalCost of your order is"+totalCostOne );
+// document.write("price of item 2 is"+priceItemTwo +"<br>"+"quantity Of item two"+quantityItemTwo +"<br>"+"shipping charges"+shippingCharges),"<br>"
+// document.write("totalCost of your order is"+ totalCostTwo)
+
+////question8
+var totalMarks   =   600
+var markObtained =   499
+var percentage =  markObtained / totalMarks *100
+console.log(percentage);
+
